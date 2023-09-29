039c4a604dc4583e1cbf60c289a61aebc709b320
paul-tes <pauasca8462@gmail.com>
2023-09-29 13:19:20 +0300

    Add fake data

---
+++ src/FakeData.js
@@ -0,0 +1,52 @@
+export const usersData = [
+  {
+    id: 1,
+    name: "John Doe",
+    userName: "johndoe"
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    userName: "janesmith"
+  },
+  // {
+  //   id: 3,
+  //   name: "Alex Johnson",
+  //   userName: "alexjohnson"
+  // },
+  // {
+  //   id: 4,
+  //   name: "Emily Brown",
+  //   userName: "emilybrown"
+  // },
+  // {
+  //   id: 5,
+  //   name: "Michael Wilson",
+  //   userName: "michaelwilson"
+  // },
+  // {
+  //   id: 6,
+  //   name: "Olivia Davis",
+  //   userName: "oliviadavis"
+  // },
+  // {
+  //   id: 7,
+  //   name: "William Taylor",
+  //   userName: "williamtaylor"
+  // },
+  // {
+  //   id: 8,
+  //   name: "Sophia Clark",
+  //   userName: "sophiaclark"
+  // },
+  // {
+  //   id: 9,
+  //   name: "James Anderson",
+  //   userName: "jamesanderson"
+  // },
+  // {
+  //   id: 10,
+  //   name: "Emma Martinez",
+  //   userName: "emmamartinez"
+  // }
+];
